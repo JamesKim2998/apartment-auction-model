bf754fa961a4cbcb037473c7990e9b1df90a55ff
김대원 <james.kim@devsisters.com>
2019-06-02 12:39:20 +0900

    auction_list

--- crawler/crawler/crawler/settings.py
+++ crawler/crawler/crawler/settings.py
@@ -19,10 +19,10 @@ NEWSPIDER_MODULE = 'crawler.spiders'
 #USER_AGENT = 'crawler (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
@@ -33,7 +33,7 @@ ROBOTSTXT_OBEY = True
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
---
+++ crawler/crawler/crawler/spiders/auction_list_spider.py
@@ -0,0 +1,33 @@
+import scrapy
+
+
+class AuctionListSpider(scrapy.Spider):
+    name = "auction_list"
+
+    def start_requests(self):
+        for year in range(2007, 2018):
+            for month in range(1, 12):
+                yield self.request(year, month, 1)
+
+    def parse(self, response):
+        xpath = '//input/@value'
+        selectors = response.xpath(xpath)
+        item_list = list(map(lambda x: x.root, selectors))[:-1]
+
+        year, month, day = response.request.date
+        date_str = f'{year}-{month:02d}-{day:02d}'
+        yield {date_str: item_list}
+
+        if day != 31:
+            new_day = day + 1
+            yield self.request(year, month, new_day)
+
+    def request(self, year, month, day):
+        url = 'http://www.auctioning.co.kr/auction/list_print.php?'
+        date_str = f'{year}-{month:02d}-{day:02d}'
+        query_str = f'ipdate1={date_str}&ipdate2={date_str}&search_usage2=101'
+
+        req = scrapy.Request(url + query_str, self.parse)
+        req.date = [year, month, day]
+        return req
+
